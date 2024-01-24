eabb54a05535ef674bd5d33f1bde2eb9fc0ab84c
oyinkan <skusman@student.lautech.edu.ng>
2024-01-24 21:00:31 +0000

    worked on creating variables, using console.log

---
+++ js/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World");
+
+// Before 2015 changes to JavaScript
+var a = 85; // assigning 85 to "a" or declaration
+var b = 5;
+
+var c = a + b;
+
+console.log(c);
+
+
+// after the 2015 changes to JavaScript
+let age = 23;
+const firstName = "Shakirah"; // create constant values
+
+age = 35; // reassigning 35 to age
+
+console.log(age);
+
+
+let height = 11.6;
+
+let canVote = false;
